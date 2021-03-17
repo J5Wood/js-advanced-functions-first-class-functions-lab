bfa98af8291a445ae83da519d8d4930313fbb2ce
Jeremy Wood <js.wood206@gmail.com>
2021-03-16 22:19:20 -0700

    commit tree 541b3198bff519062ff278af425ddacf0c5e9a08

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    const firstTwo = () => drivers.slice(0, 2)
+    return firstTwo()
+}
+
+function returnLastTwoDrivers(drivers) {
+    const lastTwo = () => drivers.slice(-2)
+    return lastTwo()
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int) {
+    return (fare) => fare * int;
+}
+
+function fareDoubler(fare) {
+    const doubledFare = createFareMultiplier(2);
+    return doubledFare(fare)
+}
+
+function fareTripler(fare) {
+    const tripledFare = createFareMultiplier(3);
+    return tripledFare(fare)
+}
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+}
